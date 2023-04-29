cd3eb2bdfa9eb7fc3b685d3a3a285bed19e77f3a
Blessium <alessandro.melgar04@gmail.com>
2023-04-29 17:01:16 +0200

    Added the database connection

---
+++ database/db.go
@@ -0,0 +1,29 @@
+package database
+
+import (
+	"github.com/blessium/porking/model"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+func ConnectDatabase() (*gorm.DB, error) {
+	dsn := "postgres://blessium:blessium@porking-database:5432/postgres"
+
+	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	if err != nil {
+		return nil, err
+	}
+
+	sqlDB, err := db.DB()
+
+	if err != nil {
+		return nil, err
+	}
+
+	db.AutoMigrate(&model.User{})
+
+	sqlDB.SetMaxOpenConns(10)
+	sqlDB.SetMaxIdleConns(5)
+
+	return db, nil
+}
