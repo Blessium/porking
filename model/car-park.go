10c8b6bc689d539666b012f83fc40845df7af694
Blessium <alessandro.melgar04@gmail.com>
2023-05-01 00:03:04 +0200

    Formatted code

--- model/car-park.go
+++ model/car-park.go
@@ -1,18 +1,15 @@
 package model
 
-import "gorm.io/gorm"
-
 type CarPark struct {
-	gorm.Model
-	ID             uint    `gorm:"primaryKey"`
-	Name           string  `json:"name"`
-	City           string  `json:"city"`
-	Country        string  `json:"country"`
-	Zipcode        string  `json:"zipcode"`
-	Latitude       float32 `json:"latitude"`
-	Longitude      float32 `json:"longitude"`
-	Description    string  `json:"description"`
-	TotalSpaces    int32   `json:"total_spaces"`
-	PricePerHour   float32 `json:"price_per_hour"`
-    ParkingSpots   []ParkingSpot 
+	ID           uint    `gorm:"primaryKey"`
+	Name         string  `json:"name"`
+	City         string  `json:"city"`
+	Country      string  `json:"country"`
+	Zipcode      string  `json:"zipcode"`
+	Latitude     float32 `json:"latitude"`
+	Longitude    float32 `json:"longitude"`
+	Description  string  `json:"description"`
+	TotalSpaces  int32   `json:"total_spaces"`
+	PricePerHour float32 `json:"price_per_hour"`
+	ParkingSpots []ParkingSpot
 }
--- model/parking-spot.go
+++ model/parking-spot.go
@@ -1,11 +1,8 @@
 package model
 
-import "gorm.io/gorm"
-
 type ParkingSpot struct {
-	gorm.Model
-	ID           uint   `gorm:"primaryKey"`
-    CarParkID    uint     
+	ID           uint `gorm:"primaryKey"`
+	CarParkID    uint
 	Type         string `json:"type"`
 	Size         string `json:"size"`
 	Availability string `json:"availability"`
