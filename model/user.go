0b6c3dbfa45d29a9cbd4468390ef7ed7a3aac9bc
Blessium <alessandro.melgar04@gmail.com>
2023-04-30 19:06:02 +0200

    Formatted code

--- model/user.go
+++ model/user.go
@@ -4,7 +4,7 @@ import "gorm.io/gorm"
 
 type User struct {
 	gorm.Model
-    ID    uint `gorm:"primaryKey"`
+	ID    uint   `gorm:"primaryKey"`
 	Name  string `json:"name"`
 	Email string `json:"email"`
 	Phone string `json:"phone"`
