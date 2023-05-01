4d90a7d16e921ad0cf691f5487efd0ba0d151d15
Blessium <alessandro.melgar04@gmail.com>
2023-05-01 23:00:22 +0200

    Working reservation system

--- handler/reservation.go
+++ handler/reservation.go
@@ -1,33 +1,54 @@
 package handler
 
 import (
+	"fmt"
 	"github.com/blessium/porking/database"
 	"github.com/blessium/porking/model"
+	"github.com/blessium/porking/utils"
 	"github.com/labstack/echo/v4"
 	"net/http"
 )
 
 func CreateReservation(c echo.Context) error {
+	var r model.ReservationRequest
 
-	return c.String(http.StatusCreated, "Created")
-}
-
-func GetAllReservations(c echo.Context) error {
-	var r []model.Reservation
+	user_id, err := utils.Extract_id_from_token(c)
+	if err != nil {
+		return c.String(http.StatusBadRequest, err.Error())
+	}
 
 	db, err := database.ConnectDatabase()
 	if err != nil {
 		return c.String(http.StatusBadRequest, err.Error())
 	}
 
-	result := db.Find(&r)
-	if result.Error != nil {
+	if err := c.Bind(&r); err != nil {
+		fmt.Println("Bruuh")
 		return c.String(http.StatusBadRequest, err.Error())
 	}
 
-	if result.RowsAffected == 0 {
-		return c.String(http.StatusNotFound, "Utente non trovato")
+    re := r.ConvertToReservation()
+	re.UserID = user_id
+    fmt.Println("What the fuck")
+	db.Save(&re)
+	return c.String(http.StatusCreated, "Created")
+}
 
+func GetAllReservations(c echo.Context) error {
+	var r []model.Reservation
+
+	user_id, err := utils.Extract_id_from_token(c)
+	if err != nil {
+		return c.String(http.StatusBadRequest, err.Error())
+	}
+
+	db, err := database.ConnectDatabase()
+	if err != nil {
+		return c.String(http.StatusBadRequest, err.Error())
+	}
+
+	if err := db.Model(&model.User{ID: user_id}).Association("Reservations").Find(&r); err != nil {
+		return c.String(http.StatusBadRequest, err.Error())
 	}
 	return c.JSON(http.StatusFound, r)
 }
--- model/reservation.go
+++ model/reservation.go
@@ -1,16 +1,55 @@
 package model
 
-import "time"
+import (
+    "strconv"
+	"time"
+)
 
 type Reservation struct {
-	ID            uint `gorm:"primaryKey"`
-	CreatedAt     time.Time
-    StartTime     time.Time `json:"start_time"`
-    EndTime       time.Time `json:"end_time"`
-    Cost          float32 `json:"cost"`
-	QRCodePath    string
-    ParkingSpotID uint
-	ParkingSpot   ParkingSpot
-	UserID        uint
-	User          User
+	ID         uint `gorm:"primaryKey"`
+	CreatedAt  time.Time
+    StartTime  time.Time `gorm:"index:,composite:time,"`
+    EndTime    time.Time`gorm:"index:,composite:time"`
+	Cost       float32  
+	QRCodePath string
+	CarID      uint
+	CarParkID  uint
+	UserID     uint `gorm:"index:idx_user"`
+}
+
+type ReservationRequest struct {
+    StartTime  UnixTime `json:"start_time"`
+    EndTime    UnixTime `json:"end_time"`
+	Cost       float32   `json:"cost"`
+	CarID      uint `json:"car_id"`
+	CarParkID  uint `json:"car_park_id"`
+}
+
+func (r *ReservationRequest) ConvertToReservation() Reservation {
+    var res Reservation
+    res.StartTime  = r.StartTime.Time
+    res.EndTime = r.EndTime.Time
+    res.Cost= r.Cost
+    res.CarID = r.CarID
+    res.CarParkID = r.CarParkID
+    return res 
+}
+
+type UnixTime struct {
+	time.Time
+}
+
+func (u *UnixTime) UnmarshalJSON(b []byte) error {
+    timestamp, err := strconv.ParseInt(string(b), 10, 64)
+    if err != nil {
+        return err
+    }
+    u.Time = time.Unix(timestamp, 0)
+
+    return nil
+}
+
+func (u *UnixTime) MarshalJSON() ([]byte, error) {
+    timestamp := u.Time.Unix()
+    return []byte(strconv.FormatInt(timestamp, 10)), nil
 }
