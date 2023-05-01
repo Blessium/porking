95d3b7a02649610803a75213f7c62a59a822f887
Blessium <alessandro.melgar04@gmail.com>
2023-05-01 18:49:02 +0200

    Added utils functions

---
+++ utils/jwt.go
@@ -0,0 +1,36 @@
+package utils
+
+import (
+	"github.com/blessium/porking/model"
+	"github.com/lestrrat-go/jwx/v2/jwk"
+	"github.com/lestrrat-go/jwx/v2/jwt"
+	"github.com/lestrrat-go/jwx/v2/jwa"
+	"time"
+)
+
+func GenerateJWT(u model.User) (string, error) {
+    now := time.Now()
+	t, err := jwt.NewBuilder().
+		Claim(`user_id`, u.ID).
+		Claim(`email`, u.Email).
+		IssuedAt(now).
+        Expiration(now.Add(5 * time.Minute)).
+		Build()
+
+	if err != nil {
+		return "", err
+	}
+
+	rawKey := []byte(`abracadabra`)
+	jwkKey, err := jwk.FromRaw(rawKey)
+	if err != nil {
+		return "", err
+	}
+
+    token, err := jwt.Sign(t, jwt.WithKey(jwa.HS256, jwkKey))
+    if err != nil {
+        return "", err
+    }
+
+	return string(token), nil
+}
