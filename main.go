07d75ad980f945b5154389e30c1dca5bee1afeb0
Blessium <alessandro.melgar04@gmail.com>
2023-05-01 18:52:17 +0200

    Bloated

--- main.go
+++ main.go
@@ -2,10 +2,13 @@ package main
 
 import (
 	"github.com/labstack/echo/v4"
+    "github.com/blessium/porking/utils"
 )
+
 
 func main() {
 	e := echo.New()
-    registerRoutes(e)
-    e.Logger.Fatal(e.Start(":1234"))
+    utils.InitValidator()
+	registerRoutes(e)
+	e.Logger.Fatal(e.Start(":1234"))
 }
